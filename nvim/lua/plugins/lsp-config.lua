0d413dbb048b8bc63523a4353169f67256f6f254
Josh Bullock <mail@joshb.me>
2024-04-14 09:47:14 -0400

    Snippets and completions:

--- nvim/lua/plugins/lsp-config.lua
+++ nvim/lua/plugins/lsp-config.lua
@@ -16,8 +16,12 @@ return {
 	{
 		"neovim/nvim-lspconfig",
 		config = function()
+			local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
 			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({})
+			lspconfig.lua_ls.setup({
+				capabilities = capabilities
+			})
 		end,
 	},
 }
