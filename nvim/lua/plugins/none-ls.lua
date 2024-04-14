2db9741e9fa0df2f85b9d9dc848bf46e20d1a2d5
Josh Bullock <mail@joshb.me>
2024-04-14 08:52:08 -0400

    set up lua styler

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -4,10 +4,10 @@ return {
 		local null_ls = require("null-ls")
 		null_ls.setup({
 			sources = {
-				null_ls.builtins.formatting.stylua
-			}
+				null_ls.builtins.formatting.stylua,
+			},
 		})
 
-		vim.keymap.set('n', '<leader>gf', vim.lsp.buf.format, {})
-	end
+		vim.keymap.set("n", "<leader>ky", vim.lsp.buf.format, {})
+	end,
 }
